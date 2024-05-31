167aa92912ea8febe8490ece47f63c23756a790e
selwjoo <moi080917@gmail.com>
2024-06-01 02:15:20 +0900

    주석 지움

--- Cam_move.cs
+++ Cam_move.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class camera : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject target;// 카메라가 따라갈 대상
-    public float moveSpeed;//카메라의 속도
+    public GameObject target;
+    public float moveSpeed;
     private Vector3 targetPosition;
 
     void Start()
@@ -17,12 +17,11 @@ public class camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target.gameObject != null)//타겟이 있으면
+        if (target.gameObject != null)
         {
-            targetPosition.Set(target.transform.position.x + 4, this.transform.position.y, this.transform.position.z); //타겟의 x,y를 똑같이 따라가고 카메라의 z값이 대상과 같으면 안보임 그래서 this 사용해서 카메라의 z값을 유지
+            targetPosition.Set(target.transform.position.x + 4, this.transform.position.y, this.transform.position.z); 
             this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
-            //this 는 카메라를 가리키기 때문에 카메라의 포지션을 바꿈 Lerp(a,b,c) a에서 b까지의 거리를 C의 속도로 움직이느 것
-            //1초에 moveSpeed 만큼 움직임 
+           
 
         }
     }
