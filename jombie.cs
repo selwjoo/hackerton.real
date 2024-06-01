5bbd08cf878cdb368e4b9771fec6b634f9d43c7b
selwjoo <moi080917@gmail.com>
2024-06-01 20:27:31 +0900

    주석 작성

--- jombie.cs
+++ jombie.cs
@@ -7,7 +7,7 @@ public class jombie : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       // Debug.Log("응애 나 애기 좀ㅣㅂ")
     }
 
     // Update is called once per frame
