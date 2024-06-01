97be7fcea74af942147e234243eaea74bc2cb523
robedcloud23777 <87563308+robedcloud23777@users.noreply.github.com>
2024-06-02 00:58:33 +0900

    총 만듦

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gun : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/scripts/GameScene.cs
+++ Assets/scripts/GameScene.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class jombie : MonoBehaviour
+public class GameScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public void ctrol()
     {
-        
+        SceneManager.LoadScene("2");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
